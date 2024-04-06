eb7b6575bbfd650fd5838b007a821216f6030c42
Wikey Cao <wikeycaoweiqi@163.com>
2024-04-06 19:47:09 +0800

    SpringMVC @RequestMapping method test

---
+++ springmvc_003_RequestMapping/src/main/java/com/wikeystudyspringmvc/controller/RequestMethodController.java
@@ -0,0 +1,19 @@
+package com.wikeystudyspringmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author: wikey
+ * @create: 2024-04-06 19:38
+ * @Description:
+ */
+@Controller
+@RequestMapping(value = "requestmethod/")
+public class RequestMethodController {
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String requestMethod() {
+        return "requestmethod";
+    }
+}
